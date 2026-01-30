6985157e9bb5956b2179d9d995b38c1fd84f3cb9
mcs999 <meancs@gmail.com>
2026-01-30 12:42:57 +0700

    finish Chat with Web

--- src/langgraphagenticai/ui/streamlitui/display_result.py
+++ src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -2,6 +2,10 @@ import streamlit as st
 from langchain_core.messages import HumanMessage,AIMessage,ToolMessage
 import json
 
+from langchain_core.messages import HumanMessage ## try to fix bug
+
+
+
 
 class DisplayResultStreamlit:
     def __init__(self,usecase,graph,user_message):
@@ -23,3 +27,21 @@ class DisplayResultStreamlit:
                             st.write(user_message)
                         with st.chat_message("assistant"):
                             st.write(value["messages"].content)
+
+        elif usecase=="Chatbot with Web":
+             # Prepare state and invoke the graph
+            initial_state = {"messages": [user_message]}
+            # initial_state = {"messages": [HumanMessage(content=user_message)]}
+            res = graph.invoke(initial_state)
+            for message in res['messages']:
+                if type(message) == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message)==ToolMessage:
+                    with st.chat_message("assistant"):
+                        st.write("Tool Call Start")
+                        st.write(message.content)
+                        st.write("Tool Call End")
+                elif type(message)==AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)
